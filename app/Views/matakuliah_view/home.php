5aeb6af798c77599aab99a562cdb58d7dcaec261
PEMWEB1 <nurkholivah1701@gmail.com>
2025-04-25 22:27:47 +0700

    add project

--- app/Views/matakuliah_view/home.php
+++ app/Views/matakuliah_view/home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nolan Studio</title>
+    <title>Nur Kholivah</title>
     <link rel="stylesheet" href="<?= base_url('assets/css/home_view.css'); ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
@@ -16,19 +16,19 @@
             <nav>
                 <ul>
                     <li><a href="#">Home</a></li>
-                    <li><a href="#">Portofolio</a></li>
-                    <li><a href="#">Mata Kuliah</a></li>
-                    <li><a href="#">Kontak</a></li>
+                    <li><a href="#Hero">Portofolio</a></li>
+                    <li><a href="#about">Mata Kuliah</a></li>
+                    <li><a href="#contact">Kontak</a></li>
                 </ul>
             </nav>
         </div>
     </header>
 
-    <section class="hero">
+    <section class="hero" id="hero">
         <div class="hero_kiri">
             <p>Portofolio</p>
-            <h2>Nama Saya Nur Kholivah</h2>
-            <p>Selamat datang di portofolio saya. Saya adalah seorang profesional yang berpengalaman dalam desain, pemrograman, serta fotografi. Dengan kemampuan saya, saya berkomitmen untuk memberikan hasil terbaik dalam setiap proyek yang saya kerjakan.</p>
+            <h2>Nur Kholivah</h2>
+            <p>Selamat datang di portofolio saya. Saya adalah mahasiswi jurusan Sistem Informasi yang memiliki minat dan keterampilan dalam desain, pemrograman, serta fotografi. Dengan latar belakang pendidikan saya, saya berkomitmen untuk menerapkan pengetahuan dan kreativitas saya dalam setiap proyek yang saya kerjakan, serta terus belajar dan berkembang di bidang teknologi informasi</p>
         </div>
         <div class="hero_kanan">
             <p>Skill saya</p>
@@ -50,7 +50,7 @@
         </div>
     </section>
 
-    <section class="about">
+    <section class="about" id="about">
         <h3>Mata Kuliah</h3>
         <div class="features">
             <?php foreach ($courses as $course): ?>
@@ -65,20 +65,18 @@
         </div>
     </section>
 
-    <section class="contact">
+    <section class="contact" id="contact">
         <h3>Kontak Saya</h3>
         <form id="myForm">
-            <label for="nama">Nama Pengontak:</label><br>
+            <label for="nama">Nama:</label><br>
             <input type="text" id="nama" name="nama" required><br><br>
-
-            <label for="perusahaan">Perusahaan:</label><br>
-            <input type="text" id="perusahaan" name="perusahaan" required><br><br>
-
+            <label for="perusahaan">Email:</label><br>
+            <input type="email" id="email" name="email" required><br><br>
             <label for="alasan">Alasan Mengontak:</label><br>
             <select id="alasan" name="alasan" required>
                 <option value="">Pilih Alasan</option>
-                <option value="memesan_web">Memesan Web</option>
-                <option value="bekerja_sama">Bekerja sama</option>
+                <option value="Agenda Lain">Agenda Lain</option>
+                <option value="bekerja_sama">Bekerja Sama</option>
                 <option value="rapat_kerja">Rapat Kerja</option>
             </select><br><br>
 
@@ -88,14 +86,14 @@
         <script>
             function kirimKeWhatsApp() {
                 var nama = document.getElementById("nama").value;
-                var perusahaan = document.getElementById("perusahaan").value;
+                var email = document.getElementById("email").value;
                 var alasan = document.getElementById("alasan").value;
 
-                if (nama && perusahaan && alasan) {
-                    var pesan = "Nama: " + nama + "\nPerusahaan: " + perusahaan + "\nAlasan: " + alasan;
+                if (nama && email && alasan) {
+                    var pesan = "Nama: " + nama + "\nEmail: " + email + "\nAlasan: " + alasan;
                     var encodedPesan = encodeURIComponent(pesan);
                     var nomorWhatsApp = "+6281275902829";
-                    var urlWhatsApp = "https://wa.me/+6281275902829" + "?text=" + encodedPesan;
+                    var urlWhatsApp = "https://wa.me/" + nomorWhatsApp + "?text=" + encodedPesan;
                     window.open(urlWhatsApp, "_blank");
                 } else {
                     alert("Harap isi semua bidang!");
@@ -103,13 +101,13 @@
             }
         </script>
         <div class="ikuti_saya">
-            <p>Ikuti saya di media sosial</p>
+            <p>Sosial Media</p>
         </div>
         <ul>
-            <li><a href="https://github.com/yourusername" target="_blank"><i class="fab fa-github"></i></a></li>
-            <li><a href="mailto:youremail@example.com"><i class="fas fa-envelope"></i></a></li>
-            <li><a href="https://www.instagram.com/yourusername" target="_blank"><i class="fab fa-instagram"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/yourusername" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://github.com/nrkholivah" target="_blank"><i class="fab fa-github"></i></a></li>
+            <li><a href="mailto:nurkholivah1701@gmail.com"><i class="fas fa-envelope"></i></a></li>
+            <li><a href="https://www.instagram.com/nrkhlvh_" target="_blank"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://www.https://www.linkedin.com/in/nur-kholivah-50a03527a/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
         </ul>
     </section>
     <footer>
