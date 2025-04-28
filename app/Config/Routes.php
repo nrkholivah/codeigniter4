0275639af28a7df042e13659bbe000cadec11195
PEMWEB1 <nurkholivah1701@gmail.com>
2025-04-28 19:16:13 +0700

    add project

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,12 +6,5 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'MataKuliah::home');
-$routes->get('/index_mataKuliah/(:num)', 'MataKuliah::index_mataKuliah/$1'); //halaman detail
-
-// yang 4 halaman 
-// $routes->get('pemweb', 'MataKuliah::pemweb');
-// $routes->get('metopen', 'MataKuliah::metopen');
-// $routes->get('sim', 'MataKuliah::sim');
-// $routes->get('rpl', 'MataKuliah::rpl');
-
+$routes->get('/index_matakuliah/(:num)', 'MataKuliah::matkul/$1'); //halaman detail
 $routes->setAutoRoute(false);
